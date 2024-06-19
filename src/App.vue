cda4d12c0e08c833dc2c469cebee62606126f597
Barcellos-Felipe <barcellossfelipe@gmail.com>
2024-06-19 08:54:09 -0400

    Organize hero section

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,27 @@
   <header class="fixed top-0 z-50 w-full">
     <NavBar />
   </header>
-  <div class="w-full h-[100dvh] bg-black bg-no-repeat opacity-30 absolute"></div>
   <!-- Hero Section -->
-  <div class="flex items-center h-[100dvh] bg-cover bg-[url('./assets/bg-main.png')] bg-center bg-no-repeat lg:max-h-[1080px]">
-    
-    <div class="text-white max-sm:mx-[7%] sm:mx-[10%] lg:mx-[15%] flex flex-col gap-10 z-10 max-sm:max-w-[80%] md:max-w-[60%] lg:max-w-[35%]">
-      <h1 class="font-montserrat max-md:text-[2.0em] md:text-[2.5em] font-bold leading-snug">SOLUÇÕES DIGITAIS AO SEU ALCANCE</h1>
-      <p class="font-montserrat text-sm leading-relaxed">Quando o assunto é encontrar maneiras de aumentar sua visibilidade no cenário digital, nós estamos preparados para lhe ajudar!</p>
-      <div class="w-full flex justify-start h-9 mt-3">
-        <Button class="w-[180px] h-[50px] text-xs/7 text-center fill-apollo font-bold" label="SAIBA MAIS"></Button>
+  <section id="inicio">
+    <div class="absolute h-[100dvh] w-full bg-black bg-no-repeat opacity-30"></div> <!-- Darken blackground -->
+    <div class="flex h-[100dvh] items-center bg-[url('./assets/bg-main.png')] bg-cover bg-center bg-no-repeat lg:max-h-[1080px]">
+      
+      <div class="z-10 flex flex-col gap-10 text-white max-sm:mx-[7%] max-sm:max-w-[80%] sm:mx-[10%] md:max-w-[60%] lg:mx-[15%] lg:max-w-[35%]">
+        <h1 class="font-montserrat font-bold leading-snug max-md:text-[2.0em] md:text-[2.5em]">SOLUÇÕES DIGITAIS AO SEU ALCANCE</h1>
+        <p class="font-montserrat text-sm leading-relaxed">Quando o assunto é encontrar maneiras de aumentar sua visibilidade no cenário digital, nós estamos preparados para lhe ajudar!</p>
+        <div class="mt-3 flex h-9 w-full justify-start">
+          <Button class="h-[50px] w-[180px] fill-apollo text-center text-xs/7 font-bold" label="SAIBA MAIS"></Button>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
   <!-- Hero Section -->
-   <div>
+  <section id="servicos">
+    <div class="flex h-[100dvh] items-center bg-[#F1] bg-cover bg-center bg-no-repeat lg:max-h-[1080px]">
+      <h2>Como podemos te ajudar?</h2>
+    </div>
+  </section>
+  <div>
     <h1>Hello</h1>
     <h1>Hello</h1>
     <h1>Hello</h1>
@@ -38,7 +45,7 @@
     <h1>Hello</h1>
     <h1>Hello</h1>
     <h1>Hello</h1>
-   </div>
+  </div>
 </template>
 
 <style scoped>
